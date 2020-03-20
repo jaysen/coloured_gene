9a1c34d187483461242ee99ede80507d5658c8e9
Jaysen Naidoo <jaysen@gmail.com>
2020-03-21 00:24:06 +0200

    block border as boolean
    + misc comments

--- gene_color.py
+++ gene_color.py
@@ -8,7 +8,7 @@ base_col = dict({'A':"red", 'C':"blue", 'G':"yellow", 'T':"green"})
 # seq: sequence of base (GTAC)
 # bs: block size (the size in pixels of each block)
 # strip: boolean strip whitespaces or not
-def tk_gene_colour(seq, bs=5, strip=True, borders=None):
+def tk_gene_colour(seq, bs=5, strip=True, borders=True):
     root = tkinter.Tk()
     
     if strip: # strip whitespaces
@@ -28,22 +28,29 @@ def tk_gene_colour(seq, bs=5, strip=True, borders=None):
         row = i // dim
         col = i % dim
         colour = base_col.get(seq[i],"black")
-        cv.create_rectangle(col*bs, row*bs, bs+col*bs, bs+row*bs, outline=borders, fill=colour)
+        if borders:
+            bordercol = "black"
+        else:
+            bordercol = colour
+        cv.create_rectangle(col*bs, row*bs, bs+col*bs, bs+row*bs, outline=bordercol, fill=colour)
     
     cv.pack()
     root.mainloop()
 
 
-# Read gene sequence from file and print color blocks:
-def tk_blocks_from_file(filepath, blocksize=6, strip=True, borders=None):
+### Read gene sequence from file and print color blocks:
+# filepath: path to text file with sequence data (GTAC)
+# blocksize: size of each colored block in pixels
+# strip: strip whitespaces or no
+# borders: use borders around each block or no
+def tk_blocks_from_file(filepath, blocksize=6, strip=True, borders=True):
     with open (filepath, "r") as gene_file:
         seq = gene_file.read()
     
-    tk_gene_colour(seq,5,strip,borders="gray10")
-    tk_gene_colour(seq,5,strip)
+    tk_gene_colour(seq, blocksize, strip, borders)
 
 
 if __name__ == "__main__":
     #tk_gene_colour("ATTTACGGCATGGTAATCCTCGAGCGTGTAGGC  G")
     #tk_gene_colour("ATTTACGGCATGGTAATCCTCGAGCGTGTAGGC GATTTACAGTCGGCATGGAGCGTGTAG GATTTACGGCATGGTAATCCTCGAGCGTGTAGGC  GATTTACGGCATGGAGCGTGTAGGATTTACGGCATGGAGCGTGTAG",10)
-    tk_blocks_from_file("covid-19.txt")
+    tk_blocks_from_file("covid-19.txt",blocksize=4)
